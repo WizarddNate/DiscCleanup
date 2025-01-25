f1f875b1390631434b8e357314946ccb03668a4b
Wizard Nate <nathanielorozco42@gmail.com>
2025-01-25 15:46:05 -0600

    First Level Built and Level Change Works
    YIPPIE!

--- Assets/Script/LevelChange.cs
+++ Assets/Script/LevelChange.cs
@@ -4,14 +4,12 @@ public class LevelChange : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (collision.CompareTag("PlayerBullet")) ;
-        {
-            Debug.Log("Bullet!");
-            //Destroy(gameObject);
-        }
         if (collision.CompareTag("Player")) ;
         {
+            if (collision.CompareTag("PlayerBullet"))
+            {
+                return;
+            }
             Debug.Log("Next Level!");
             //next level
             GameManager.instance.NextLevel();
